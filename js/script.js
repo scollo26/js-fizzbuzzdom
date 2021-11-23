54770aef6b3719cbe5749cacdf90c1b98418d636
scollo26 <giovanniscollo26@gmail.com>
2021-11-23 16:06:05 +0100

    aggiunto-commenti

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
 
-const ul = document.querySelector('.quadrati');
-
-
-
-
+// variabile div-contenitore
 const container = document.querySelector('.container');
 
 for(let i=1; i<=100; i++){
+    // creazione-div
     const div = document.createElement('div');
+    // contatore-dentro div
     div.append(i);
+    // classe-box-div
     div.classList.add('box');
+    // div-dentro-contenitore
     container.append(div);
 
-        if( i % 15 == 0){
-        
-        div.innerHTML ="fizzbuzz";
-        div.classList.add("red");
-        }else if( i % 5 == 0){
+    if( i % 15 == 0){
+    // stampa-fizzbuzz
+    div.innerHTML ="fizzbuzz";
+    // classe-red-a-div
+    div.classList.add("red");
+    }else if( i % 5 == 0){
                 
-        div.innerHTML ="buzz";
-        div.classList.add("yellow");
-        } else if( i % 3 == 0){
-        div.innerHTML ="fizz";
-        div.classList.add("green");
-        }
-        console.log(i);
-        div.classList.add("blue");
+    div.innerHTML ="buzz";
+    div.classList.add("yellow");
+    } else if( i % 3 == 0){
 
+    div.innerHTML ="fizz";
+    div.classList.add("green");
+    }
+    console.log(i);
     div.classList.add("blue");
+
 } 
