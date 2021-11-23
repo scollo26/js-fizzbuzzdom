130d4128819f7327835a94aab5108b3f33d5f0dc
scollo26 <giovanniscollo26@gmail.com>
2021-11-23 16:49:00 +0100

    numero random bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -3,26 +3,31 @@ const button = document.querySelector('button');
 button.addEventListener("click", function(){
 
     const ul = document.querySelector('.quadrati');
+    
+    // reset-ul
+    ul.innerHTML='';
 
 
     
 
     for(let i=1; i<= 100; i++){
+        // numero-random
+        const rand= Math.floor(Math.random() * 101);
         const li = document.createElement("li");
-        li.append(i);
+        li.append(rand);
         li.classList.add("box");
         ul.append(li);
 
 
-        if( i % 15 == 0){
+        if( rand % 15 == 0){
             
             li.innerHTML ="fizzbuzz"
             li.classList.add("red")
-        }else if( i % 5 == 0){
+        }else if( rand % 5 == 0){
             
             li.innerHTML ="buzz"
             li.classList.add("yellow")
-        } else if( i % 3 == 0){
+        } else if( rand % 3 == 0){
             console.log('fizz');
             li.innerHTML ="fizz"
             li.classList.add("green")
