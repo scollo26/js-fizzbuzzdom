ff92d6e16ba3986125837563f31a2e352e2d6b85
scollo26 <giovanniscollo26@gmail.com>
2021-11-23 16:44:27 +0100

    bottone-bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,30 +1,37 @@
+const button = document.querySelector('button');
 
-const ul = document.querySelector('.quadrati');
+button.addEventListener("click", function(){
 
-for(let i=1; i<= 100; i++){
-    const li = document.createElement("li");
-    li.append(i);
-    li.classList.add("box");
-    ul.append(li);
+    const ul = document.querySelector('.quadrati');
 
 
-    if( i % 15 == 0){
-        
-        li.innerHTML ="fizzbuzz"
-        li.classList.add("red")
-    }else if( i % 5 == 0){
-        
-        li.innerHTML ="buzz"
-        li.classList.add("yellow")
-    } else if( i % 3 == 0){
-        console.log('fizz');
-        li.innerHTML ="fizz"
-        li.classList.add("green")
-    }
-    console.log(i);
-    li.classList.add("blue")
     
-}
+
+    for(let i=1; i<= 100; i++){
+        const li = document.createElement("li");
+        li.append(i);
+        li.classList.add("box");
+        ul.append(li);
+
+
+        if( i % 15 == 0){
+            
+            li.innerHTML ="fizzbuzz"
+            li.classList.add("red")
+        }else if( i % 5 == 0){
+            
+            li.innerHTML ="buzz"
+            li.classList.add("yellow")
+        } else if( i % 3 == 0){
+            console.log('fizz');
+            li.innerHTML ="fizz"
+            li.classList.add("green")
+        }
+        console.log(i);
+        li.classList.add("blue")
+        
+    }
+});
 
 
 
