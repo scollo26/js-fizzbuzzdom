18ff1e1befc3c18fc7a6acb0937076ece8f479d1
scollo26 <giovanniscollo26@gmail.com>
2021-11-23 15:20:18 +0100

    eliminazione commenti

--- js/script.js
+++ js/script.js
@@ -1,31 +1,7 @@
 
 const ul = document.querySelector('.quadrati');
 
-// for(let i=1; i<= 100; i++){
-//     const li = document.createElement("li");
-//     li.append(i);
-//     li.classList.add("box");
-//     ul.append(li);
 
-
-//     if( i % 15 == 0){
-        
-//         li.innerHTML ="fizzbuzz"
-//         li.classList.add("red")
-//     }else if( i % 5 == 0){
-        
-//         li.innerHTML ="buzz"
-//         li.classList.add("yellow")
-//     } else if( i % 3 == 0){
-//         console.log('fizz');
-//         li.innerHTML ="fizz"
-//         li.classList.add("green")
-//     }
-//     console.log(i);
-//     li.classList.add("blue")
-    
-// }
-// container.append(div);
 
 
 const container = document.querySelector('.container');
